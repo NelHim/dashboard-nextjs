265a883f258f6bff8285ab1bb84eca31ce129aeb
NelHim <n.himbaza@alustudent.com>
2024-09-12 19:43:28 +0200

    feat: add data fetching, static & dynamic rendering, streaming, and partial prerendering

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,6 @@
 import SideNav from "@/app/ui/dashboard/sidenav";
+import { tree } from "next/dist/build/templates/app-page";
+export const experimental_ppr = true;
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
